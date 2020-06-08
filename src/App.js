ab115649172a134fc5bd2e8c57246dc6048d71c2
ashfaqmbd <ashfaqmbd@gmail.com>
2020-06-08 23:07:33 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function App(props) {
+  return (<div>Hello World of {props.arg}</div>);
+}
+
+export default App;
---
+++ src/Myapp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Myapp(){
+    return(<h2>Welcome to my New App content</h2>);
+}
+export default Myapp;
